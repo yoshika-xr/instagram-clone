fbe69606f3314ea622bb6ed4ad84928fe1f4aed0
yoshika-xr <yoshikajune@gmail.com>
2025-12-23 13:46:06 +0530

    first step

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+ function Header(){
+    return(
+        <>
+        <div>
+        <li>Home</li>
+        <li>Post</li>
+        <li></li>
+        <li></li>
+        <li></li>
+        </div>
+        </>
+    )
+ }
