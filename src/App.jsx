eb103de9a430f65440a786637a6914252fb6934a
yoshika-xr <yoshikajune@gmail.com>
2025-12-23 20:25:13 +0530

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import Sidebar from './components/Sidebar';
 
 
 
@@ -7,7 +8,7 @@ function App() {
   return (
     <>
      <div className="d-flex vh-100">
-      <div className='w-20'>Sidebar</div>
+      <div className='w-20'><Sidebar/></div>
       <div className='w-50'>Feed</div>
       <div className='w-30'>Suggestion</div>
      </div>
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+function Sidebar() {
+  return (
+    <div className="m-3">
+      <div className="d-flex flex-column gap-3">
+        <img className="textimg" src="src\assets\textimages.png" alt="textimages"/>
+        <div><i className="bi bi-house-door-fill"></i>Home</div>
+        <div><i className="bi bi-compass"></i>Explore</div>
+        <div><i className="bi bi-search"></i>Search</div>
+        <div><i className="bi bi-play-circle"></i>Reels</div>
+        <div><i className="bi bi-chat-dots"></i>Messages</div>
+        <div><i className="bi bi-heart"></i>Notification</div>
+        <div><i className="bi bi-plus-circle"></i>Create</div>
+        <div><i className="bi bi-person-circle"></i>profile</div>
+      </div>
+      <div className="position-fixed bottom-0 d-flex flex-column gap-3 mb-3">
+        <div><i className="bi bi-threads"></i>Threads</div>
+        <div><i className="bi bi-list"></i>More</div>
+      </div>
+    </div>
+  );
+}
+export default Sidebar;
