2abe0692ed999b5cda3d4ca1fb78d4048a908138
yoshika-xr <yoshikajune@gmail.com>
2025-12-28 21:27:28 +0530

    step 7

--- src/components/Suggestion.jsx
+++ src/components/Suggestion.jsx
@@ -2,14 +2,19 @@ import React, { useEffect, useState } from "react";
 
 function Suggestion() {
   const [profile, setProfile] = useState(null);
-  const [suggestion,setsuggestion]=useState([])
-  
+  const [suggestion, setsuggestion] = useState([]);
+
   useEffect(() => {
-    fetch("http://localhost:3000/posts")
+    fetch("http://localhost:3000/profile")
       .then((data) => data.json())
-      .then((data) => setProfile(data)).
-      catch(error => console.log(err));
-  },[]);
+      .then((data) => setProfile(data))
+      .catch((err) => console.log(err));
+
+    fetch("http://localhost:3000/profile")
+      .then((data) => data.json())
+      .then((data) => setsuggestion(data))
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <div>
       <div className="d-flex">
