8f2cbf02c7f26ab6b02f749c0a19b240b0092087
yoshika-xr <yoshikajune@gmail.com>
2026-01-07 18:52:35 +0530

    step11

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,34 +1,45 @@
 import React, { useEffect, useState } from "react";
 
 function Posts() {
-  const [posts, setPosts] =useState([]);
-  useEffect(()=>{
-    fetch('http://localhost:3000/posts').
-    then((data) => data.json()).
-    then((data) => setPosts(data)).
-    catch(err => console.log(err));
-  },[]);
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/posts")
+      .then((data) => data.json())
+      .then((data) => setPosts(data))
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
-    <div className="d-flex justify-content-center">{posts.length > 0 ? (<div>
-      {posts.map((post)=>(
-        <div className="my-3" key={post.id}>
-          <div className="d-flex">
-            <img className="dp rounded-circle" src={post.user.profile_pic} alt="profilepic"/>
-            <h6 className="mt-2">{post.user.username}</h6>
-          </div>
-          <div><img className="image" src={post.image} alt=""  /></div>
-          <div>
-            <i className="bi bi-heart"></i>
-            <i className="bi bi-chat"></i>
-            <i className="bi bi-send"></i>
-          </div>
-          <b>{post.likes} Likes</b>
-          <div>{post.caption}</div>
-          
+    <div className="d-flex justify-content-center">
+      {posts.length > 0 ? (
+        <div>
+          {posts.map((post) => (
+            <div className="my-3" key={post.id}>
+              <div className="d-flex">
+                <img
+                  className="dp rounded-circle"
+                  src={post.user.profile_pic}
+                  alt="profilepic"
+                />
+                <h6 className="mt-2">{post.user.username}</h6>
+              </div>
+              <div>
+                <img className="image" src={post.image} alt="" />
+              </div>
+              <div>
+                <i className="bi bi-heart"></i>
+                <i className="bi bi-chat"></i>
+                <i className="bi bi-send"></i>
+              </div>
+              <b>{post.likes} Likes</b>
+              <div>{post.caption}</div>
+            </div>
+          ))}
         </div>
-      ))}
-      </div>):( <div>loading...</div>)}</div>
+      ) : (
+        <div>loading...</div>
+      )}
+    </div>
   );
 }
 
--- src/components/Suggestion.jsx
+++ src/components/Suggestion.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 
 function Suggestion() {
   const [profile, setProfile] = useState(null);
-  const [suggestion, setSuggestion] = useState([]);
+  const [suggestions, setSuggestion] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:3000/profile")
@@ -18,20 +18,39 @@ function Suggestion() {
   return (
     <div>
       <div className="suggestion w-75 m-4">
-      {profile ? (
-        <div className="d-flex">
-          <img
-            className="dp rounded-circle"
-            src={profile.profile_pic}
-            alt="profilepic"
+        {profile ? (
+          <div className="d-flex">
+            <img
+              className="dp rounded-circle"
+              src={profile.profile_pic}
+              alt="profilepic"
             />
-          <h6 className="mt-2">{profile.username}</h6>
-          <small className="ms-auto text-primary">Switch</small>
-          <p>suggestion for you</p><b>See All</b>
+            <h6 className="mt-2">{profile.username}</h6>
+            <small className="ms-auto text-primary">Switch</small>
+          </div>
+        ) : (
+          <p>loading...</p>
+        )}
+        <div className="d-flex m-3">
+          <p>suggestion for you</p>
+          <b className="ms-auto">See All</b>
         </div>
-      ) : (
-        <p>loading...</p>
-      )}
+
+        {suggestions .length > 0 ? (
+          <div>{suggestions.map((suggestion) => {
+            <div key={suggestion.id}>
+              <div>
+                <img
+                  className="dp rounded-circle"
+                  src={suggestion.profile_pic}
+                  alt="profilepic"
+                />
+                <h6 className="mt-2">{suggestion.username}</h6>
+              </div>
+            </div>
+          })}
+            </div>
+        ):(<p>loading...</p>)}
       </div>
     </div>
   );
