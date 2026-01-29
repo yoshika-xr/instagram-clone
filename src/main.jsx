6cd5c97b581a8eb742d41e2ffead3a9220ea7b62
yoshika-xr <yoshikajune@gmail.com>
2026-01-29 20:25:01 +0530

    step16

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import View from './components/View.jsx'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 
+const router=createBrowserRouter([
+  {
+    path:'/',
+    element:<App/>
+  },
+  {
+    path:'/view',
+    element:<View />
+  }
+])
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+   <RouterProvider router={router} />
 )
