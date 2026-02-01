bac74d4c1b7edd8bfd90302223c7e3b1a79490de
yoshika-xr <yoshikajune@gmail.com>
2026-02-01 18:52:11 +0530

    step19

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -3,20 +3,20 @@ import { useParams } from "react-router-dom";
 
 function View() {
   const { id } = useParams();
-  const [vstory, setVstory] = useState(null);
+  const [story, setstory] = useState(null);
+
   useEffect(() => {
     fetch(`http://localhost:3000/story/${id}`)
       .then((data) => data.json())
-      .then((data) => setVstory(data))
+      .then((data) => setstory(data))
       .catch((err) => console.log(err));
   }, []);
 
   return (
     <div>
-      {vstory ? (
+      {story ? (
         <div>
-          {/* <img src={vstory.image}></img> */}
-          {vstory.profile_pic}
+          <img src={story.user.profile_pic}></img>
         </div>
       ) : (
         <div>Loading....</div>
