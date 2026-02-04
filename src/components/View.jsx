fdfc5f0639b7f2d1e00f5428786a7bf4c1b6d1b3
yoshika-xr <yoshikajune@gmail.com>
2026-02-04 07:44:54 +0530

    step21

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,24 +1,28 @@
 import React, { useState, useEffect } from "react";
-import { useParams ,Link} from "react-router-dom";
+import { useParams ,Link,Navigate} from "react-router-dom";
 
 function View() {
-  const { id } = useParams();
+  const { id,tot } = useParams();
   const [story, setstory] = useState(null);
+  const navigate=
 
   useEffect(() => {
     fetch(`http://localhost:3000/story/${id}`)
       .then((data) => data.json())
       .then((data) => setstory(data))
       .catch((err) => console.log(err));
-  }, []);
+  }, [id]);
 
+  if(id >tot){
+    
+  }
   return (
     <div>
       {story ? 
         <div className="d-flex justify-content-center align-items-center m-2">
-           <Link><i class="bi bi-arrow-left-circle-fill"></i></Link>
+           <Link to={`http://localhost:5173/view/${Number(id)-1}/${tot}`}><i class="bi bi-arrow-left-circle-fill"></i></Link>
          <img className="vh-100 " src={story.image} alt="story"></img>
-          <Link><i class="bi bi-arrow-right-circle-fill"></i></Link>
+          <Link to={`http://localhost:5173/view/${Number(id)+1}/${tot}`}><i class="bi bi-arrow-right-circle-fill"></i></Link>
         </div>
        : 
         <div>Loading....</div>
--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ const router = createBrowserRouter([
     element:<App />
   },
   {
-    path:'/view/:id',
+    path:'/view/:id/?tot',
     element:<View />
   }
 ])
