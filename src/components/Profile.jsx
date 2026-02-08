0a4d047c6a9627d46b007c53c1587942373bfce8
yoshika-xr <yoshikajune@gmail.com>
2026-02-08 22:24:05 +0530

    step23

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { data } from "react-router-dom";
 
 function Profile() {
-  return (
-    <div>Profile</div>
-  )
+  const [profile, setProfile] = useState(null);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/profile')
+    .then((data) => {setProfile(data.data)});
+    console.log(data);
+  });
+  return <div>Profile</div>;
 }
 
-export default Profile
+export default Profile;
